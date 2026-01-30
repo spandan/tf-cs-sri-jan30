f81d8f0eb0d048e57a416296c78b6272f42b28f8
oplingerpaul-cloud <oplingerpaul@gmail.com>
2026-01-30 20:35:52 +0000

    update

--- main.tf
+++ main.tf
@@ -1,11 +1,25 @@
-resource "aws_instance" "user14" {
-  ami="ami-0993d3759749c153c"
-  #ami=var.ami-id
-  #ami=data.aws_ami.amazon_linux_2023.id
-  instance_type = var.instance_type
-  
-  tags = {
-    Name = var.tag_name
-    owner=var.tag_owner
- }
+provider "aws" {
+  region = "us-west-1"
+}
+
+module "Vms_app1" {
+  source = "git::https://github.com/spandan/tf-sri-01302026//ec2-instance?ref=main"
+  region-1 = "us-west-1"
+  instance_type = "t3.micro"
+  subnets = ["subnet-084785ce11bb38827", "subnet-0e100c7e306c1812d"]
+  num_of_vm = 1
+  env = "dev"
+}
+
+output "vm_private_ips01" {
+  value = module.Vms_app1.vm_priv_pips
+}
+module "Web_SG01" {
+  source = "git::https://github.com/spandan/tf-sri-01302026//ec2-instance?ref=main"
+  project = "vishwa"
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_vpc" "default" {
+  default = true
 }
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "vm_private_ips01" {
+  value = module.Vms_app1.vm_priv_pips
+}
